32eb027e5f8ebf7e5e66ad021e4f14bb07a4d486
nafkhanzam <nafkhanalzamzami@gmail.com>
2019-09-12 21:45:52 +0700

    Added ConsoleApp template & added run.sh

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+package com.nafkhanzam;
+
+import static com.nafkhanzam.Console.*;
 
 public class Main {
     public static void main(String[] args) {
-        
+        outln("Hello, world!");
     }
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -1,4 +1,5 @@
+package com.nafkhanzam;
 
 public class Matrix {
-    
+
 }
