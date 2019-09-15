3f489d230c2dc50fdfcc63975ba63172b309598a
naufal-dean <naufal.dean@gmail.com>
2019-09-15 20:22:52 +0700

    Add interpolation.

--- src/Point.java
+++ src/Point.java
@@ -1,27 +1,53 @@
+package tubes;
+
+import java.util.*;
 
 public class Point {
-    public float x, y;
+    private double x, y;
 
     //** Konstruktor **//
-    public Point(float x, float y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
+    //** Selektor **//
+    //-- Selektor: Get --//
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    //-- Selektor: Set --//
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
     // ** Interpolasi **//
-    // public static Matrix toMatrix(Point[] p) {
-    //     double[][] tempVal = new double[p.length][p.length+1];
-    //
-    //     for (int i = 0; i < p.length; i++) {
-    //         for (int j = 0; j < p.length; j++) {
-    //
-    //         }
-    //     }
-    // }
-    //
-    // public static SPL interpolatePoint(Point[] p) {
-    //
-    // }
+    public static Matrix toMatrix(Point[] p) {
+        double[][] tempVal = new double[p.length][p.length+1];
+
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p.length; j++) {
+                tempVal[i][j] = Math.pow(p[i].getX(), j);
+            }
+            tempVal[i][p.length] = p[i].getY();
+        }
+        return new Matrix(tempVal);
+    }
+
+    public static SPL interpolatePoint(Point[] p) {
+        Matrix m = Point.toMatrix(p);
+
+        return m.getSistemPersamaanLinear(Method.GAUSS);
+    }
 
     //** Utility **//
     @Override
