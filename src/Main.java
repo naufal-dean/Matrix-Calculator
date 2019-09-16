bdef9a1c3c8fd254021445073a0220bebcbdb1f3
nafkhanzam <nafkhanalzamzami@gmail.com>
2019-09-16 19:56:19 +0700

    fixed SPL

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 package tubes;
 
+
 import static tubes.Console.*;
 
 public class Main {
@@ -9,66 +10,73 @@ public class Main {
         // System.out.printf("%.100f", 0d);
     }
     public static void main2() {
-        outln("Hello, world!");
-        outln("TEST");
-        double[][] test = { { 1, 2, 3, 2 }, { 1, 2, 6, 2 }, { 2, 4, 9, 4 },{1,2,3,2} };
-        double[][] test2 = { { 1 }, { 1 }, { 1 }, { 2 }};
-        double[] buffer;
-        Matrix M = new Matrix(test);
-        Matrix Ma = new Matrix(test2);
-        System.out.println("Normal: ");
-        M.tulisMatrix(); System.out.println();
-        //System.out.printf("Determinan Cramer: %.20f %b\n", M.getDeterminan(Method.CRAMER), 0 == M.getDeterminan(Method.CRAMER));
-        //System.out.printf("Determinan Gauss: %.20f %b\n\n", M.getDeterminan(Method.GAUSS), 0 == M.getDeterminan(Method.GAUSS));
-        System.out.println("Entry matrix: ");
-        //M.getEntryMatrix(2, 2).tulisMatrix(); System.out.println();
-        //System.out.println("Cofactor matrix: ");
-        //M.getCofactorMatrix().tulisMatrix(); System.out.println();
-        System.out.println("Adjoint matrix: ");
-        //M.getAdjointMatrix().tulisMatrix(); System.out.println();
-        // System.out.println("Inverse Gauss-Jordan: ");
-        // M.getInverseMatrix(Method.GAUSS_JORDAN).tulisMatrix(); System.out.println();
-        // System.out.println("Inverse Cramer: ");
-        // M.getInverseMatrix(Method.CRAMER).tulisMatrix(); System.out.println();
-        System.out.println("Echelon Form: ");
-        Matrix x = M.getEchelonFormCC(M.getMaxColumn()); System.out.println();
-        M.getEchelonFormCC(M.getMaxColumn()).tulisMatrix(); System.out.println();
-        x.getEchelonFormCC(x.getMaxColumn()).tulisMatrix(); System.out.println();
-        System.out.println("Reduced Echelon Form: ");
-        M.getReducedEchelonForm(M.getMaxColumn()).tulisMatrix(); System.out.println();
+        // outln("Hello, world!");
+        // outln("TEST");
+        // double[][] test = { { 1, 2, 3, 2 }, { 1, 2, 6, 2 }, { 2, 4, 9, 4 },{1,2,3,2} };
+        // double[][] test2 = { { 1 }, { 1 }, { 1 }, { 2 }};
+        // double[] buffer;
+        // Matrix M = new Matrix(test);
+        // Matrix Ma = new Matrix(test2);
+        // System.out.println("Normal: ");
+        // M.tulisMatrix(); System.out.println();
+        // //System.out.printf("Determinan Cramer: %.20f %b\n", M.getDeterminan(Method.CRAMER), 0 == M.getDeterminan(Method.CRAMER));
+        // //System.out.printf("Determinan Gauss: %.20f %b\n\n", M.getDeterminan(Method.GAUSS), 0 == M.getDeterminan(Method.GAUSS));
+        // System.out.println("Entry matrix: ");
+        // //M.getEntryMatrix(2, 2).tulisMatrix(); System.out.println();
+        // //System.out.println("Cofactor matrix: ");
+        // //M.getCofactorMatrix().tulisMatrix(); System.out.println();
+        // System.out.println("Adjoint matrix: ");
+        // //M.getAdjointMatrix().tulisMatrix(); System.out.println();
+        // // System.out.println("Inverse Gauss-Jordan: ");
+        // // M.getInverseMatrix(Method.GAUSS_JORDAN).tulisMatrix(); System.out.println();
+        // // System.out.println("Inverse Cramer: ");
+        // // M.getInverseMatrix(Method.CRAMER).tulisMatrix(); System.out.println();
+        // System.out.println("Echelon Form: ");
+        // Matrix x = M.getEchelonFormCC(M.getMaxColumn()); System.out.println();
+        // M.getEchelonFormCC(M.getMaxColumn()).tulisMatrix(); System.out.println();
+        // x.getEchelonFormCC(x.getMaxColumn()).tulisMatrix(); System.out.println();
+        // System.out.println("Reduced Echelon Form: ");
+        // M.getReducedEchelonForm(M.getMaxColumn()).tulisMatrix(); System.out.println();
 
-       /* double[][] test3 = { { 1, 0, 0, 0, 1 }, { 0, 1, 0, 0, 1 }, { 0, 0, 0, 1, 1 }, { 0, 0, 0, 0, 0 } };
+        double[][] test3 = { { 1, 2, 3, 4, 5 }, 
+                             { 1, 2, 3, 4, 5 }, 
+                             { 1, 2, 3, 4, 5 }, 
+                             { 1, 2, 3, 4, 5 } };
         Matrix M3 = new Matrix(test3);
-        System.out.println("Matriks SPL: ");
-        M3.tulisMatrix();
-        outln("jadi gini:");
-        M3.getReducedEchelonForm(M3.getMaxColumn()-1).tulisMatrix();
-        System.out.println("Get Solution Cramer: ");
-        out(M3.getSistemPersamaanLinear(Method.CRAMER));
-        outln("====");
-        out(M3.getSistemPersamaanLinear(Method.CRAMER));
-        outln("====");
-        out(M3.getSistemPersamaanLinear(Method.GAUSS));
-        outln("====");
+        out(M3);
+        outln("-");
+        out(M3.getReducedEchelonForm(M3.getMaxColumn()-1));
         out(M3.getSistemPersamaanLinear(Method.GAUSS_JORDAN));
-        outln("====");
-        out(M3.getSistemPersamaanLinear(Method.INVERSE));
-        outln("====");*/
-        // buffer = M3.getSistemPersamaanLinear(Method.CRAMER);
-        // for (int i = 1; i < buffer.length; i++) {
-        //     System.out.printf("x%d = %.20f\n", i, buffer[i]);
-        // }
+        // System.out.println("Matriks SPL: ");
+        // M3.tulisMatrix();
+        // outln("jadi gini:");
+        // M3.getReducedEchelonForm(M3.getMaxColumn()-1).tulisMatrix();
+        // System.out.println("Get Solution Cramer: ");
+        // out(M3.getSistemPersamaanLinear(Method.CRAMER));
+        // outln("====");
+        // out(M3.getSistemPersamaanLinear(Method.CRAMER));
+        // outln("====");
+        // out(M3.getSistemPersamaanLinear(Method.GAUSS));
+        // outln("====");
+        // out(M3.getSistemPersamaanLinear(Method.GAUSS_JORDAN));
+        // outln("====");
+        // out(M3.getSistemPersamaanLinear(Method.INVERSE));
+        // outln("====");
+        // // buffer = M3.getSistemPersamaanLinear(Method.CRAMER);
+        // // for (int i = 1; i < buffer.length; i++) {
+        // //     System.out.printf("x%d = %.20f\n", i, buffer[i]);
+        // // }
 
-     //   System.out.println();
-       // System.out.println("Get Solution Inverse: ");
+        // System.out.println();
+        // System.out.println("Get Solution Inverse: ");
 
-//        out(M3.getSistemPersamaanLinear(Method.INVERSE));
-        // buffer = M3.getSistemPersamaanLinear(Method.INVERSE);
-        // for (int i = 1; i < buffer.length; i++) {
-        //     System.out.printf("x%d = %.20f\n", i, buffer[i]);
-        // }
+        // out(M3.getSistemPersamaanLinear(Method.INVERSE));
+        // // buffer = M3.getSistemPersamaanLinear(Method.INVERSE);
+        // // for (int i = 1; i < buffer.length; i++) {
+        // //     System.out.printf("x%d = %.20f\n", i, buffer[i]);
+        // // }
 
-  //      System.out.println();
+        // System.out.println();
 
         // outln(M);
         // outln(-0f == 0f);
--- src/SPL.java
+++ src/SPL.java
@@ -1,5 +1,8 @@
 package tubes;
 
+
+
+
 import static tubes.Console.*;
 
 public class SPL {
@@ -12,6 +15,9 @@ public class SPL {
     // content[1][2] = 2;
 
     public SPL(Matrix m) {
+        // outln("BEFORE");
+        // out(m);
+        // outln("AFTER");
         initREF(m.getReducedEchelonForm(m.getMaxColumn()-1));
     }
 
@@ -21,28 +27,67 @@ public class SPL {
             content[i][0] = sol[i];
     }
 
-    private void initREF(Matrix m) {
-        int r = m.getMaxRow(), c = m.getMaxColumn()-1;
+    // private void initREF(Matrix m) {
+    //     int r = m.getMaxRow(), c = m.getMaxColumn()-1;
+    //     for (int i = c+1; i <= r; i++)
+    //         if (m.getElement(i, m.getMaxColumn()) != 0)
+    //             throw new RuntimeException("Matrix is inconsistent!");
+    //     r = c;
+    //     content = new double[c+1][c+1];
+    //     while (r >= 1) {
+    //         int j = 1;
+    //         boolean found = false;
+    //         while (j <= c) {
+    //             if (m.getElement(r, j) != 0) {
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
+    //         content[j][0] = m.getElement(r, c+1);
+    //         if (!found) {
+    //             // outln(r + "," + j + "," + (c+1));
+    //             if (m.getElement(r, c+1) != 0)
+    //                 throw new RuntimeException("Matrix is inconsistent!");
+    //             content[j][j] = 1;
+    //         } else
+    //             for (int j2 = j+1; j2 <= c; j2++) {
+    //                 double v2 = m.getElement(r, j2);
+    //                 if (v2 != 0)
+    //                     content[j][j2] = -v2;
+    //             }
+    //         r--;
+    //     }
+    // }
+
+    public void initREF(Matrix in) {
+        int r = in.getMaxRow(), c = in.getMaxColumn()-1;
         for (int i = c+1; i <= r; i++)
-            if (m.getElement(i, m.getMaxColumn()) != 0)
+            if (in.getElement(i, in.getMaxColumn()) != 0)
                 throw new RuntimeException("Matrix is inconsistent!");
-        r = c;
         content = new double[c+1][c+1];
-        for (int j = c; j >= 1; j--) {
-            double v = m.getElement(r, j);
-            content[j][0] = m.getElement(r, c+1);
-            if (v == 0) {
-                outln(r + "," + j + "," + (c+1));
-                if (m.getElement(r, c+1) != 0)
-                    throw new RuntimeException("Matrix is inconsistent!");
+        Matrix m = new Matrix(c, c+1);
+        for (int i = 1; i <= in.getMaxRow(); i++)
+            for (int j = 1; j <= c+1; j++)
+                m.setElement(i, j, in.getElement(i, j));
+        int j = 1;
+        for (int i = 1; i <= c; i++) {
+            if (j > c)
+                break;
+            if (m.getElement(i, j) == 0) {
+                // outln(i + "," + j + " = " + 0);
                 content[j][j] = 1;
-            } else
-                for (int j2 = j+1; j2 <= c; j2++) {
-                    double v2 = m.getElement(r, j2);
-                    if (v2 != 0)
-                        content[j][j2] = -v2;
+                j++;
+                i--;
+                continue;
+            }
+            content[j][0] = m.getElement(i, c+1);
+            // outln(i + "," + j + " = " + m.getElement(i, j));
+            for (int j2 = j+1; j2 <= c; j2++)
+                if (m.getElement(i, j2) != 0) {
+                    content[i][j2] = -m.getElement(i, j2);
+                    outln(i + "," + j2 + " = " + m.getElement(i, j2));
                 }
-            r--;
+            j++;
         }
     }
 
@@ -51,21 +96,27 @@ public class SPL {
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < content.length; i++) {
             sb.append("x" + i + " = ");
+            boolean found = false;
             for (int j = 0; j < content[i].length; j++) {
                 double v = content[i][j];
-                if (v == 0)
+                if (v == 0 && j > 0)
                     continue;
-                if (j > 0) {
-                    if (v < 0) {
-                        v = -v;
-                        sb.append(" - ");
-                    } else
+                    if (j > 0 && found) {
+                        if (v < 0) {
+                            v = -v;
+                            sb.append(" - ");
+                        } else
                         sb.append(" + ");
-                }
-                sb.append(v);
+                    }
+                if (v != 0)
+                    found = true;
+                if (v != 0)
+                    sb.append(String.format("(%.2f)", v));
                 if (j > 0)
                     sb.append("s" + j);
             }
+            if (!found)
+                sb.append(String.format("(%.2f)", content[i][0]));
             sb.append("\n");
         }
         return sb.toString();
